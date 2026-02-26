c9483fafa8af697055eab52bdb8ce1461f86ac25
Mithun M Nair <165287396+Narutomithun@users.noreply.github.com>
2026-02-26 05:46:51 +0000

    seletorscss

---
+++ C-Programs/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.HashMap;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();    
+        sc.nextLine();            
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String title = sc.next();  
+            int id = sc.nextInt();      
+            map.put(title, id);
+        }
+        int q = sc.nextInt();     
+        sc.nextLine();            
+        for (int i = 0; i < q; i++) {
+            String search = sc.nextLine();
+            if (map.containsKey(search)) {
+                System.out.println(map.get(search));
+            } else {
+                System.out.println("Book Not Found");
+            }
+        }
+         System.out.println("MITHUN M NAIR 0128");
+    }
+}
---
+++ Leetcode/daily.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class daily {
+    public static void main(String[] args){
+        String s ="1010";
+        int k =2;
+        double length=Math.pow(2,k);
+        System.out.println(length);
+        System.out.println(s.length());
+        if(s.length()<length){
+            System.out.println("False");
+            System.out.println("False");
+        }
+        else{
+            System.out.println("True");
+        }
+        
+
+    }
+}
